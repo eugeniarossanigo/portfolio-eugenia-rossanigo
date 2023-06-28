473075a72c3adfe103d82c08e0385827e96363d5
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-06-28 13:44:05 -0300

    canvas changed

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,13 @@
 export default function Canvas({ size }) {
   
   return (
-    <section className='absolute top-4 left-1 flex flex-col gap-2'>
-      <canvas className={`bg-[#2A4858] w-${size} h-${size} rounded-full`}></canvas>
-      <canvas className={`bg-[#205072] rounded-full w-${size} h-${size}`}></canvas>
-      <canvas className={`bg-green rounded-full w-${size} h-${size}`}></canvas>
-      <canvas className={`bg-[#56C596] rounded-full w-${size} h-${size}`}></canvas>
-      <canvas className={`bg-[#7BE495] rounded-full w-${size} h-${size}`}></canvas>
-      <canvas className={`bg-[#CFF4D2] rounded-full w-${size} h-${size}`}></canvas>
+    <section className='absolute top-4 left-3 flex flex-col gap-2'>
+      <div className={`bg-[#2A4858] w-${size} h-${size} rounded-full`}></div>
+      <div className={`bg-[#205072] w-${size} h-${size} rounded-full`}></div>
+      <div className={`bg-green w-${size} h-${size} rounded-full`}></div>
+      <div className={`bg-[#56C596] w-${size} h-${size} rounded-full`}></div>
+      <div className={`bg-[#7BE495] w-${size} h-${size} rounded-full`}></div>
+      <div className={`bg-[#CFF4D2] w-${size} h-${size} rounded-full`}></div>
     </section>
   )
 }
