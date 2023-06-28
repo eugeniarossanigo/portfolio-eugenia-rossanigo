9c643627bdd50cdecc467268649a85fe9568510d
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-06-28 12:30:43 -0300

    data test

---
+++ src/data/jobs.js
@@ -0,0 +1,18 @@
+export const jobs = [
+  {
+      "id": 1,
+      "title": "Mentora Junior",
+      "datestart": "enero 2023",
+      "datefinish": "la actualidad",
+      "company": "Argentina Programa",
+      "description": "• Planificación de actividades del curso y dictado de workshops. • Revisión y calificación de las tareas entregables. • Seguimiento a los alumnos velando por su progreso técnico. • Incentivar un ambiente de aprendizaje colaborativo y de respeto."
+  },
+  {
+    "id": 2,
+    "title": "Web Scraper Python",
+    "datestart": "junio 2014",
+    "datefinish": "junio 2022",
+    "company": "Bright Sector",
+    "description": "• Desarrollo de programas de crawleo en Python. • Responsable de la calidad de datos obtenidos. • Personas a cargo en equipo de trabajo."
+  },
+]
---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+      "id": 1,
+      "title": "Project 1",
+      "date": "junio 2023",
+      "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit deleniti eveniet accusamus modi nesciunt debitis nemo nam atque perferendis maxime, soluta corrupti voluptatem fugit praesentium iste alias consectetur ducimus eligendi!",
+      "github": "https://github.com/eugeniarossanigo",
+      "link": "#",
+      "grupal": true
+  },
+  {
+    "id": 2,
+    "title": "Project 2",
+    "date": "junio 2023",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit deleniti eveniet accusamus modi nesciunt debitis nemo nam atque perferendis maxime, soluta corrupti voluptatem fugit praesentium iste alias consectetur ducimus eligendi!",
+    "github": "https://github.com/eugeniarossanigo",
+    "link": "#",
+    "grupal": true
+  },
+  {
+    "id": 3,
+    "title": "Project 3",
+    "date": "junio 2023",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit deleniti eveniet accusamus modi nesciunt debitis nemo nam atque perferendis maxime, soluta corrupti voluptatem fugit praesentium iste alias consectetur ducimus eligendi!",
+    "github": "https://github.com/eugeniarossanigo",
+    "link": "#",
+    "grupal": true
+  },
+  {
+    "id": 4,
+    "title": "Project 4",
+    "date": "junio 2023",
+    "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit deleniti eveniet accusamus modi nesciunt debitis nemo nam atque perferendis maxime, soluta corrupti voluptatem fugit praesentium iste alias consectetur ducimus eligendi!",
+    "github": "https://github.com/eugeniarossanigo",
+    "link": "#",
+    "grupal": true
+  }
+]
