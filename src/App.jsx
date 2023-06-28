27cc9647ce478845d4eb95bd7865b261698b89db
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-06-28 12:29:06 -0300

    app routes and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
-import Home from './pages/Home'
+import ProjectContainer from './components/ProjectContainer';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import WorkContainer from './components/WorkContainer';
 
 function App() {
   return (
-    <>
+    <div id='main-layout' className='font-montserrat'>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <>
+              <Welcome />
+              <ProjectContainer />
+              <WorkContainer />
+              {/* <main className='flex flex-row'>
+                <Container />
+                <Container />
+              </main> */}
+            </>
+          } />
+          {/* <Route path='/projects' element={<Projects />} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
