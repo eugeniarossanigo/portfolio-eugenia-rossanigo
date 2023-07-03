ba43fd450ffa299ec1c5902de4463ffa15f4c9b7
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-03 13:18:19 -0300

    components updated

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,26 +1,15 @@
 import { projects as data } from '../data/projects'
-import Card from './Card'
-import Canvas from './Canvas';
+import CardContainer from './CardContainer'
+import CardProject from './CardProject'
+import Container from './Container';
 
 export default function ProjectContainer() {
   return (
-    <section id='projects' className='w-full relative flex flex-col justify-center items-center my-10 py-4 gap-4'>
-      {data.map((item) => <Card key={item.id} task={item}>
-        <header>
-          <p className='text-[10px]'>{item.date}</p>
-          <h3 className='text-green font-extrabold'>{item.title}</h3>
-        </header>
-        <p className='text-sm'>{item.description}</p>
-        <nav className='flex items-center gap-2'>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            <img className='w-[20px]' src='/github.png' alt='github' />
-          </a>
-          <a href='http://' target='_blank' rel='noopener noreferrer'>
-            <img className='w-[24px]' src='/link.png' alt='link' />
-          </a>
-        </nav>
-      </Card>)}
-      <Canvas size='7'/>
-    </section>
+    <Container title='Projects' size='5'>
+      {data.map((item) =>
+        <CardContainer key={item.id}>
+          <CardProject item={item}/>
+        </CardContainer>)}
+    </Container>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,22 +1,24 @@
 import Canvas from './Canvas';
+import { useState, useEffect } from 'react'
 
 export default function Welcome() {
+
   return (
-    <section className='w-full relative h-[95vh] flex justify-center items-center py-10 gap-4'>
+    <section className='w-full relative h-[95vh] flex justify-center items-center gap-4'>
       <img className='w-52 border border-light-grey rounded-full p-2' src="/cv-pic.jpg" alt="portfolio-pic" />
       <div className='flex flex-col gap-2'>
-        <h1 className='text-4xl font-extrabold'><span className='text-green'>E</span>ugenia <span className='text-green'>R</span>ossanigo</h1>
-        <h2 className='text-2xl font-semibold'>Full Stack developer</h2>
+        <h1 className='text-5xl font-extrabold text-strong-grey'><span className='text-green'>E</span>ugenia <span className='text-green'>R</span>ossanigo</h1>
+        <h2 className='text-3xl font-semibold'>Full Stack developer</h2>
         <nav className='flex gap-2'>
           <a href='http://' target='_blank' rel='noopener noreferrer'>
-            <img className='w-[32px]' src='/linkedin.png' alt='linkedin' />
+            <img className='w-[32px]' src='/linkedin-green.png' alt='linkedin' />
           </a>
           <a href='http://' target='_blank' rel='noopener noreferrer'>
-            <img className='w-[32px]' src='/github.png' alt='github' />
+            <img className='w-[32px]' src='/github-green.png' alt='github' />
           </a>
         </nav>
       </div>
-      <Canvas size='7'/>
+      <Canvas size='5'/>
     </section>
   )
 }
