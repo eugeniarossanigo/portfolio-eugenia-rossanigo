bbc58fa380d6999d88908327ca36c0681fc3c75a
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-03 13:17:12 -0300

    files deleted

--- src/components/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }) {
-  return (
-    <article className='w-[80%] border border-[#329d9b73] rounded-2xl flex flex-col p-5 gap-2'>
-      { children }
-    </article>
-  )
-}
