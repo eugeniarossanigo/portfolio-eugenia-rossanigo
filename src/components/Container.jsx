f6f06e49a81d85669baff4c812be7070071de82d
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-03 22:46:46 -0300

    links and title changed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,8 +1,8 @@
 import Canvas from './Canvas';
 
-export default function Container({ title, size, children }) {
+export default function Container({ link, title, size, children }) {
   return (
-    <section id={`${title.toLowerCase()}`} className='w-full relative flex flex-col justify-center items-center mb-14 pt-20 gap-4'>
+    <section id={link} className='w-full relative flex flex-col justify-center items-center mb-14 pt-20 gap-4'>
       { children }
       <Canvas title={title} size={size} />
     </section>
