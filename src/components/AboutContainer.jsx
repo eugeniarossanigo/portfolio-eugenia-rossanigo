8dc18998ccc719df490d325d3ff90e0597534548
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-04 12:45:23 -0300

    hover buttons

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,8 +6,8 @@ export default function AboutContainer() {
       <article className='w-[80%] px-10'>
         <p className='text-sm'>Me desenvuelvo en el ambiente IT desde hace un tiempo y estos últimos años decidí capacitarme en nuevas tecnologías porque me gustan los desafíos. Es mi objetivo seguir creciendo en este rubro para poder desarrollar todo mi potencial, habilidades y aportar mi conocimiento y entusiasmo a nuevos proyectos.</p>
         <nav className='flex justify-start gap-5 py-5'>
-          <a className='w-[110px] bg-green text-sm text-white font-bold text-center rounded-md py-2' href="#">CV Español</a>
-          <a className='w-[110px] bg-[#7BE495] text-sm text-white font-bold text-center rounded-md py-2' href="#">CV Inglés</a>
+          <a className='w-[110px] bg-green hover:bg-[#2A4858] hover:scale-105 text-sm text-white font-bold text-center rounded-md py-2' href="#">CV Español</a>
+          <a className='w-[110px] bg-[#56C596] hover:bg-[#2A4858] hover:scale-105 text-sm text-white font-bold text-center rounded-md py-2' href="#">CV Inglés</a>
         </nav>
       </article>
     </Container> 
