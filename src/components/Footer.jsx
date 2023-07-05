e1a39d48c33c27fa8dd9a2aa6ce5757862a26079
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-05 15:51:17 -0300

    change text footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className='w-full h-[4vh] text-xs flex justify-center items-center border-lightGrey border-t'>
-      Portfolio hecho con: React.js
+      Todos los derechos reservados © 2023
     </div>
   )
 }
