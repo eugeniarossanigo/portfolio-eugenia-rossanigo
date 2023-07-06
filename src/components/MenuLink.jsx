93faca8e05b0ed25e8c7dabd83ef7307c182722b
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-05 22:06:46 -0300

    update hover

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,5 +1,5 @@
 export default function MenuLink({ item }) {
   return (
-    <a href={`#${item.href}`} className='hover:text-strongGrey'>{item.name}</a>
+    <a href={`#${item.href}`} className='hover:text-green'>{item.name}</a>
   )
 }
