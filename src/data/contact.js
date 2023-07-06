1c0f7e195e42d908e43bb96b365527fda8c311d3
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-06 13:38:52 -0300

    contact data

---
+++ src/data/contact.js
@@ -0,0 +1,27 @@
+export const contact = [
+  {
+    href: 'mailto:eugeniarossanigo@gmail.com',
+    src: '/email-gray.png',
+    text: 'eugeniarossanigo@gmail.com'
+  },
+  {
+    href: 'tel:+5493415819753',
+    src: '/phone-gray.png',
+    text: '+54 341 5819753'
+  },
+  {
+    href: 'https://www.linkedin.com/in/eugenia-rossanigo/',
+    src: '/linkedin-gray.png',
+    text: 'eugenia-rossanigo'
+  },
+  {
+    href: 'https://github.com/eugeniarossanigo',
+    src: '/github-gray.png',
+    text: 'eugeniarossanigo'
+  },
+  {
+    href: 'https://goo.gl/maps/3uvXX7bPDjY36F3a8',
+    src: '/pin-gray.png',
+    text: 'Rosario, Argentina'
+  },
+]
