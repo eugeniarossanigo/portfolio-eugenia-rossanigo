cadf8315776633d612a28d3b8212e11cb1ab94dd
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-11 00:54:27 -0300

    canvas map colors

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,17 +1,16 @@
+const colors = ["[#2A4858]", "[#205072]", "green", "[#56C596]", "[#7BE495]", "[#CFF4D2]"]
+
 export default function Canvas({ title }) {
   
   return (
-    <section className='absolute top-3 left-3 flex flex-col'>
+    <section className='absolute top-3 left-4 flex flex-col'>
       {
         title && <h2 className='text-2xl font-bold pb-8'>{title}</h2>
       }
       <div className='flex flex-col gap-2'>
-        <div className='bg-[#2A4858] w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
-        <div className='bg-[#205072] w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
-        <div className='bg-green w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
-        <div className='bg-[#56C596] w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
-        <div className='bg-[#7BE495] w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
-        <div className='bg-[#CFF4D2] w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full'></div>
+        {colors.map((color, index) => (
+          <div key={index} className={`bg-${color} w-5 sm:w-6 lg:w-7 h-5 sm:h-6 lg:h-7 rounded-full`}></div>
+        ))}
       </div>
     </section>
   )
