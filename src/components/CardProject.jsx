4f4f2f13019007745627d3ae09997cbae0cd812d
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-11 00:30:35 -0300

    container width changed and card datas updated

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -9,10 +9,16 @@ export default function CardProject({ item}) {
         <h3 className='text-base text-green font-extrabold'>{item.title}</h3>
       </header>
       <p className='text-sm'>{item.description}</p>
+      <p className='text-xs'>Hecho con: <span className='italic'>{item.techs}</span></p>
       <nav className='flex items-center gap-2'>
-        <a href='http://' target='_blank' rel='noopener noreferrer'>
+        <a href={item.gitFront} target='_blank' rel='noopener noreferrer'>
           <SiGithub className='text-green w-[22px] h-[22px] hover:scale-105 hover:text-colorChanged' />
         </a>
+        { item.gitBack &&
+          <a href={item.gitBack} target='_blank' rel='noopener noreferrer'>
+            <SiGithub className='text-lightGray w-[22px] h-[22px] hover:scale-105 hover:text-colorChanged' />
+          </a>
+        }
         <a href='http://' target='_blank' rel='noopener noreferrer'>
           <HiLink className='bg-darkGray text-insideBg w-[24px] h-[24px] rounded-full p-1 hover:scale-105 hover:bg-colorChanged' />
         </a>
