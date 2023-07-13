76f31dbf71d317663e602640f553d03d0ab8d4cb
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-13 20:09:19 -0300

    add projects links

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,31 @@
 export const projects = [
   {
-    title: "MINDY PetShop",
-    date: "agosto 2022",
-    description: "• E-commerce de artículos de veterinaria.",
-    gitFront: "https://github.com/eugeniarossanigo/petshop-mindy",
-    link: "#",
-    techs: "Javascript, Bootstrap",
+    title: "⚈ CAFÉ",
+    date: "octubre 2022",
+    description: "• Web y mobile app. • E-commerce de café y productos relacionados. • Equipo de 5 personas, encargada de detalles de diseño, funcionalidades front y backend.",
+    gitFront: "https://github.com/eugeniarossanigo/dot-cafe-front",
+    gitBack: "https://github.com/eugeniarossanigo/dot-cafe-back",
+    link: "https://dot-cafe-front.vercel.app/",
+    techs: "React.js, Node.js, Express.js, MongoDB, Bootstrap",
     grupal: true
   },
   {
     title: "Mytinerary",
     date: "septiembre 2022",
     description: "• Web y mobile app sobre itinerarios de viajes. • Equipo de 2 personas, encargada de diseño, funcionalidades front y backend.",
-    gitFront: "https://github.com/eugeniarossanigo/mytinerary-almost-hackers",
+    gitFront: "https://github.com/eugeniarossanigo/mytinerary-front",
     gitBack: "https://github.com/eugeniarossanigo/mytinerary-back",
-    link: "#",
+    link: "https://mytinerary-front-sand.vercel.app/",
     techs: "React.js, Node.js, Express.js, MongoDB",
     grupal: true
-},
+  },
   {
-    title: "⚈ CAFÉ",
-    date: "octubre 2022",
-    description: "• Web y mobile app. • E-commerce de café y productos relacionados. • Equipo de 5 personas, encargada de detalles de diseño, funcionalidades front y backend.",
-    gitFront: "https://github.com/eugeniarossanigo/dot-cafe-front",
-    gitBack: "https://github.com/eugeniarossanigo/dot-cafe-back",
-    link: "#",
-    techs: "React.js, Node.js, Express.js, MongoDB, Bootstrap",
+    title: "MINDY PetShop",
+    date: "agosto 2022",
+    description: "• E-commerce de artículos de veterinaria.",
+    gitFront: "https://github.com/eugeniarossanigo/petshop-mindy",
+    link: "https://petshop-mindy-iota.vercel.app/",
+    techs: "Javascript, Bootstrap",
     grupal: true
-},
+  },
 ]
